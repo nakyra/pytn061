9cdbbfe29129296575cb133b5c967010378f827a
stefkwan-h8 <schristine@hacktiv8.com>
2021-08-31 22:00:34 +0700

    sesi 3

---
+++ .ipynb_checkpoints/matematik-checkpoint.py
@@ -0,0 +1,9 @@
+pi = 3.14
+golden_ratio = 1.618
+
+def nambah(a,b):
+    return a+b
+
+def cekGenap(num):
+    return num % 2 == 0
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# PYTN061
+## Sesi 1 - Data Types
+## Sesi 2 - If, Loop
+## Sesi 3 - Function, Module, Package
