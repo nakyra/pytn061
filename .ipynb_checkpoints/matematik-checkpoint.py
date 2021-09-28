85a26eb1fbbb3e3ae8a13b09fae37e1a7e3dc9f9
stefkwan-h8 <schristine@hacktiv8.com>
2021-09-29 00:19:33 +0700

    remove cached folders

--- .ipynb_checkpoints/matematik-checkpoint.py
+++
@@ -1,9 +0,0 @@
-pi = 3.14
-golden_ratio = 1.618
-
-def nambah(a,b):
-    return a+b
-
-def cekGenap(num):
-    return num % 2 == 0
-
--- .ipynb_checkpoints/readme-checkpoint.md
+++
@@ -1,5 +0,0 @@
-# PYTN061
-- Sesi 1 - Data Types
-- Sesi 2 - If, Loop
-- Sesi 3 - Function, Module, Package
-- Sesi 4 - Numpy
